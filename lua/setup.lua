b3c1e59cfdca9136ddbf3b02d96a11eb1d41a9e6
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-10-05 20:47:09 +0200

    feat: add format-on-save

--- lua/setup.lua
+++ lua/setup.lua
@@ -46,6 +46,10 @@ lsp.rust_analyzer.setup {
 }
 -- Lua
 lsp.lua_ls.setup {}
+-- Format-on-save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function() vim.lsp.buf.format() end,
+})
 
 -- Autocomplete
 local luasnip = require("luasnip")
