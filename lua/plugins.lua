87ffa34f2ee182c090b6a822bb1d6bf634e053e2
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-10-13 19:16:14 +0200

    add plugins and format stuff

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,78 +1,85 @@
 return {
-  { -- GitHub colorschemes
-    "projekt0n/github-nvim-theme",
-    config = function()
-      require("github-theme").setup {
-        options = {
-          transparent = true,
-        },
-      }
-    end,
-  },
-  { -- LSP configuration
-    "neovim/nvim-lspconfig",
-  },
-  -- More autocomplete
-  "hrsh7th/cmp-nvim-lsp",
-  "hrsh7th/cmp-buffer",
-  "hrsh7th/cmp-path",
-  "hrsh7th/cmp-cmdline",
-  { -- Autocomplete
-    "hrsh7th/nvim-cmp",
-  },
-  { -- Snippets
-    "L3MON4D3/LuaSnip",
-  },
-  "p00f/nvim-ts-rainbow",
-  { -- Treesitter
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("nvim-treesitter.configs").setup {
-        -- Languages
-        ensure_installed = { "rust", "lua", "markdown", "markdown_inline", "scheme" },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        rainbow = {
-          enable = true,
-        },
-      }
-    end,
-  },
-  { -- Telescope
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-  },
-  { -- Editorconfig
-    "editorconfig/editorconfig-vim",
-  },
-  { -- File explorer (tree)
-    "nvim-tree/nvim-tree.lua",
-  },
-  { -- Lua line (status line)
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup {
-        options = {
-          icons_enabled = false,
-          theme = "wombat",
-        },
-      }
-    end,
-  },
-  { -- Comments
-    "numToStr/Comment.nvim",
-    opts = {
-      toggler = {
-        line = "<leader>cl", -- Comment line
-        block = "<leader>cb", -- Comment block
-      },
-      opleader = {
-        line = "<leader>cl", -- Comment line
-        block = "<leader>cb", -- Comment block
-      },
+    {
+        -- GitHub colorschemes
+        "projekt0n/github-nvim-theme",
+        config = function()
+            require("github-theme").setup {
+                options = {
+                    transparent = true,
+                },
+            }
+        end,
     },
-  },
+    -- Tokyo Night colorschemes
+    "folke/tokyonight.nvim",
+    { -- LSP configuration
+        "neovim/nvim-lspconfig",
+    },
+    -- More autocomplete
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    { -- Autocomplete
+        "hrsh7th/nvim-cmp",
+    },
+    { -- Snippets
+        "L3MON4D3/LuaSnip",
+    },
+    "p00f/nvim-ts-rainbow",
+    {
+        -- Treesitter
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                -- Languages
+                ensure_installed = { "rust", "lua", "markdown", "markdown_inline", "scheme" },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                rainbow = {
+                    enable = true,
+                },
+            }
+        end,
+    },
+    {
+        -- Telescope
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+    },
+    { -- Editorconfig
+        "editorconfig/editorconfig-vim",
+    },
+    { -- File explorer (tree)
+        "nvim-tree/nvim-tree.lua",
+    },
+    {
+        -- Lua line (status line)
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup {
+                options = {
+                    icons_enabled = false,
+                    theme = "wombat",
+                },
+            }
+        end,
+    },
+    {
+        -- Comments
+        "numToStr/Comment.nvim",
+        opts = {
+            toggler = {
+                line = "<leader>cl",  -- Comment line
+                block = "<leader>cb", -- Comment block
+            },
+            opleader = {
+                line = "<leader>cl",  -- Comment line
+                block = "<leader>cb", -- Comment block
+            },
+        },
+    },
 }
--- lua/setup.lua
+++ lua/setup.lua
@@ -2,7 +2,7 @@ local opt = vim.opt
 local keymap = vim.keymap
 
 -- Visuals
-vim.cmd.colorscheme("github_dark")
+vim.cmd.colorscheme("tokyonight-night")
 opt.number = true
 opt.relativenumber = true
 
@@ -12,131 +12,130 @@ opt.tabstop = 4
 opt.shiftwidth = 4
 
 -- Autocomplete
-opt.completeopt = {'menu', 'menuone', 'noselect'}
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
 local lsp = require("lspconfig")
 local lsp_defaults = lsp.util.default_config
 lsp_defaults.capabilities = vim.tbl_deep_extend(
-  "force",
-  lsp_defaults.capabilities,
-  require("cmp_nvim_lsp").default_capabilities()
+    "force",
+    lsp_defaults.capabilities,
+    require("cmp_nvim_lsp").default_capabilities()
 )
 
 -- Keybinds
 local telescope = require("telescope.builtin")
-keymap.set("n", "<leader>ff", telescope.find_files, {}) -- Find files
-keymap.set("n", "<leader>f/", telescope.live_grep, {}) -- Find in files
-keymap.set("n", "<leader>,", telescope.buffers, {}) -- Find buffer
-keymap.set("n", "<leader>fh", telescope.help_tags, {}) -- Find help
+keymap.set("n", "<leader>ff", telescope.find_files, {})    -- Find files
+keymap.set("n", "<leader>f/", telescope.live_grep, {})     -- Find in files
+keymap.set("n", "<leader>,", telescope.buffers, {})        -- Find buffer
+keymap.set("n", "<leader>fh", telescope.help_tags, {})     -- Find help
 local tree = require("nvim-tree.api")
-keymap.set("n", "<leader>ft", tree.tree.toggle, {}) -- File explorer
-keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {}) -- Hover
-keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {}) -- Rename
+keymap.set("n", "<leader>ft", tree.tree.toggle, {})        -- File explorer
+keymap.set("n", "<leader>ch", vim.lsp.buf.hover, {})       -- Hover
+keymap.set("n", "<leader>cr", vim.lsp.buf.rename, {})      -- Rename
 keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {}) -- Code actions
-keymap.set("n", "<leader>cx", telescope.diagnostics, {}) -- Diagnostics
+keymap.set("n", "<leader>cx", telescope.diagnostics, {})   -- Diagnostics
 local cmp = require("cmp")
-keymap.set("i", "<C-Space>", cmp.complete, {}) -- Autocomplete
+keymap.set("i", "<C-Space>", cmp.complete, {})             -- Autocomplete
 keymap.set("n", "<bar>", vim.cmd.vsplit, {})
 keymap.set("n", "<bslash>", vim.cmd.split, {})
 
 -- LSP
 -- Rust analyzer
 lsp.rust_analyzer.setup {
-  capabilities = capabilities,
-  settings = {
-    ["rust-analyzer"] = {},
-  },
+    capabilities = capabilities,
+    settings = {
+        ["rust-analyzer"] = {},
+    },
 }
 -- Lua
 lsp.lua_ls.setup {}
 -- Format-on-save
 vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function() vim.lsp.buf.format() end,
+    callback = function() vim.lsp.buf.format() end,
 })
 
 -- Autocomplete
 local luasnip = require("luasnip")
-local select_opts = {behavior = cmp.SelectBehavior.Select}
+local select_opts = { behavior = cmp.SelectBehavior.Select }
 cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  sources = {
-    { name = "path" },
-    { name = "nvim_lsp", keyword_length = 1 },
-    { name = "buffer", keyword_length = 3 },
-    { name = "luasnip", keyword_length = 2 },
-  },
-  window = {
-    documentation = cmp.config.window.bordered()
-  },
-  formatting = {
-    fields = {'menu', 'abbr', 'kind'},
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = 'λ',
-        luasnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫',
-      }
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = {
+        { name = "path" },
+        { name = "nvim_lsp", keyword_length = 1 },
+        { name = "buffer",   keyword_length = 3 },
+        { name = "luasnip",  keyword_length = 2 },
+    },
+    window = {
+        documentation = cmp.config.window.bordered()
+    },
+    formatting = {
+        fields = { 'menu', 'abbr', 'kind' },
+        format = function(entry, item)
+            local menu_icon = {
+                nvim_lsp = 'λ',
+                luasnip = '⋗',
+                buffer = 'Ω',
+                path = '🖫',
+            }
 
-      item.menu = menu_icon[entry.source.name]
-      return item
-    end,
-  },
-  mapping = {
-    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+            item.menu = menu_icon[entry.source.name]
+            return item
+        end,
+    },
+    mapping = {
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
 
-    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
-    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
 
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<C-y>'] = cmp.mapping.confirm({select = true}),
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
 
-    ['<C-f>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(1) then
-        luasnip.jump(1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
+        ['<C-f>'] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
 
-    ['<C-b>'] = cmp.mapping(function(fallback)
-      if luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
+        ['<C-b>'] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
 
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      local col = vim.fn.col('.') - 1
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            local col = vim.fn.col('.') - 1
 
-      if cmp.visible() then
-        cmp.select_next_item(select_opts)
-      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        fallback()
-      else
-        cmp.complete()
-      end
-    end, {'i', 's'}),
+            if cmp.visible() then
+                cmp.select_next_item(select_opts)
+            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, { 'i', 's' }),
 
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item(select_opts)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-  },
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item(select_opts)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    },
 }
 
 -- Other
 opt.clipboard = "unnamedplus" -- Always use system clipboard
-
